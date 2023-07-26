2356459783592c76dcdf6b6c0b1f8001f27ddd51
TwylaWeiyuTang <twyla9876@outlook.com>
2023-07-26 17:29:46 +0200

    set up redux for managing cell adding, deleting, moving actions

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
@@ -18,24 +19,66 @@ const initialState: CellState = {
   data: {},
 };
 
-const reducer = (
-  state: CellState = initialState,
-  action: Action
-): CellState => {
+const reducer = produce((state: CellState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload;
+      state.data[id].content = content;
       return state;
+
     case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      // create a brand new order array by assigning all the orders we didn't delete
+      state.order = state.order.filter((id) => id !== action.payload);
       return state;
 
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => {
+        id === action.payload.id;
+      });
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+      // boundary check to see if target index is within range
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
       return state;
+
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      // if we didn't find the index of the cell where above we want to insert the new cell
+      // we add it to the end of the list
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
       return state;
 
     default:
       return state;
   }
+});
+
+// random id generation function
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
 };
 
 export default reducer;
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,5 +2,28 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducers/cellsReducer";
 import reducers from "./reducers";
+import { ActionType } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: "text",
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: "code",
+//   },
+// });
+
+// const id = store.getState().cells?.order[0]
+
+// const state = store.getState();
+
+// console.log(state);
